fd603c71382f25fb062ad84b7298bc95d81c8861
smoelius1 <86530199+smoelius1@users.noreply.github.com>
2021-08-16 08:19:01 -0400

    Create entrypoint.sh

---
+++ action/entrypoint.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -eu
+
+export PYTHONPATH=/root/.local/lib/python3.6/site-packages
+
+OPTIONS="contract config"
+
+CMD=(echidna-test "$INPUT_FILES")
+
+for OPTION in OPTIONS; do
+    VAR="INPUT_${OPTION^^}"
+    CMD+=(--$OPTION ${!VAR})
+done
+
+echo "${CMD[@]}"
+
+"${CMD[@]}"
